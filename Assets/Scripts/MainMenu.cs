02c9f6fbc07f1d1cbf51e8fdb2a14275d8d608bf
KevinPhu1999 <1999kevinphu@gmail.com>
2023-03-06 17:35:52 +1100

    Added script for main menu to play game

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void PlayGame()
     {
-        
+        //Makes it so once the button is pressed, it will play the next scene in order
+        //make sure to edit or change the build index around for this to work!!!
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
