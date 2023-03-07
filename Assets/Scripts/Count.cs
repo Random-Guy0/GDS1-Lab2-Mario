35b2ff29dc650d9499f906d1c31581ae2c4a3b20
CoveringSky1 <barryli12138@gmail.com>
2023-03-08 01:52:49 +1100

    flag update

---
+++ Assets/Scripts/Count.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Count : MonoBehaviour
+{
+    public GameObject manager;
+    public bool firstTouch = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (firstTouch)
+        {
+            int score = 100;
+            int i = (int)collision.transform.position.y;
+            if(i != -2) {
+                if (i != -1)
+                {
+                    score += (i + 1) * 700;
+                }
+                Debug.Log(score);
+                firstTouch = false;
+            }
+        }
+    }
+}
