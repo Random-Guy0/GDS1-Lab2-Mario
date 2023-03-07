c085dc2f70fb28b9ae6b85186de1c2040a3b3062
Amren Gill <amren.gill@gmail.com>
2023-03-08 00:04:48 +1100

    Player health/lives script

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerStats : MonoBehaviour
+{
+    [SerializeField] private BoxCollider2D playerCollider;
+    [SerializeField] private int lives;
+    
+    private PowerupState powerupState;
+
+    private void Start()
+    {
+        powerupState = PowerupState.Small;
+    }
+
+    public void OneUp()
+    {
+        lives++;
+    }
+
+    public int GetLives()
+    {
+        return lives;
+    }
+
+    public void CollectPowerup()
+    {
+        switch (powerupState)
+        {
+            case PowerupState.Small:
+                powerupState = PowerupState.Big;
+                playerCollider.size = new Vector2(1, 2);
+                playerCollider.offset = new Vector2(0, 0.5f);
+                break;
+            case PowerupState.Big:
+                powerupState = PowerupState.Flower;
+                break;
+            case PowerupState.Flower:
+                break;
+        }
+    }
+    
+    public void TakeDamage()
+    {
+        if (powerupState == PowerupState.Small)
+        {
+            lives--;
+            if (lives == 0)
+            {
+                //Game over
+            }
+        }
+        else
+        {
+            powerupState = PowerupState.Small;
+            playerCollider.size = Vector2.one;
+            playerCollider.offset = Vector2.zero;
+        }
+    }
+}
+
+public enum PowerupState
+{
+    Small,
+    Big,
+    Flower
+}
