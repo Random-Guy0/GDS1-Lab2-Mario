1807db50271e3686e9c057540eaa4353ce45c179
CoveringSky1 <barryli12138@gmail.com>
2023-03-09 06:23:02 +1100

    Finish

--- Assets/Finish.cs
+++ Assets/Finish.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Finish : MonoBehaviour
 {
+    public GameObject Text;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +19,9 @@ public class Finish : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        Application.Quit();
+        Time.timeScale = 0;
+        Debug.Log("Finish");
+        Text.SetActive(true);
     }
 }
