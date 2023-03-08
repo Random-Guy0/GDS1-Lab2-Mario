867f69a04802be445b2cab84f1aa450e6c64552b
Ruben <madcrafty@icloud.com>
2023-03-08 23:37:09 +1100

    KOOPA WORK
    KOOPA KOOPA

--- Assets/Script/Enemy2.cs
+++ Assets/Script/Enemy2.cs
@@ -7,27 +7,46 @@ public class Enemy2 : MonoBehaviour
     public int health;
     public float walkSpeed;
     public float shellSpeed;
+    float curSpeed;
+    public float RespawnTime;
+    float curRespawnTime;
     public Color walkColour;
     public Color shellColour;
-    public GameObject effect;
     public LayerMask lm;
     public RaycastHit2D[] results;
-    bool shell;
+    bool shell = false;
+    bool move = false;
     Collider2D[] damageBox;
     Rigidbody2D rb;
-    Vector3 effectSpawnOffset = new Vector3(0, -0.25f, 0);
+    SpriteRenderer sr;
     // Start is called before the first frame update
     void Start()
     {
-        walkSpeed = -walkSpeed;
+        curSpeed = -walkSpeed;
         damageBox = transform.GetChild(0).GetComponentsInChildren<Collider2D>();
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(walkSpeed * Time.deltaTime, 0, 0));
+        if (!shell)
+        {
+            transform.Translate(new Vector3(curSpeed * Time.deltaTime, 0, 0));
+        }
+        else if (move)
+        {
+            transform.Translate(new Vector3(curSpeed * Time.deltaTime, 0, 0));
+        }
+        else if (!move)
+        {
+            curRespawnTime += Time.deltaTime;
+            if (curRespawnTime >= RespawnTime)
+            {
+                ShellMode();
+            }
+        }
     }
     bool IsPlayerTouching(Collider2D player)
     {
@@ -53,37 +72,93 @@ public class Enemy2 : MonoBehaviour
         switch (type)
         {
             case 1:
-                // shell mode
+                ShellMode();
                 return;
             default:
                 break;
         }
         Destroy(gameObject);
     }
+    void ShellMode()
+    {
+        shell = !shell;
+        curRespawnTime = 0;
+        if (shell)
+        {
+            sr.color = shellColour;
+            curSpeed = 0;
+        }
+        else
+        {
+            sr.color = walkColour;
+            curSpeed = -walkSpeed;
+        }
+    }
+    void ShellMove(bool direction)
+    {
+        move = !move;
+        curRespawnTime = 0;
+        if (direction)
+        {
+            curSpeed = shellSpeed;
+        }
+        else
+        {
+            curSpeed = -shellSpeed;
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (!shell)
         {
-            if (collision.attachedRigidbody.velocity.y < 0 || !collision.IsTouching(damageBox[0]))
+            if (collision.CompareTag("Player"))
             {
-                Damage(1);
+                if (collision.attachedRigidbody.velocity.y < 0 || !collision.IsTouching(damageBox[0]))
+                {
+                    Damage(1);
+                }
+                else
+                {
+                    Debug.Log(name + ": deal damage");
+                }
             }
-            else
+        }
+        else
+        {
+            if (collision.CompareTag("Player"))
             {
-                Debug.Log(name + ": deal damage");
+                if (collision.attachedRigidbody.velocity.y < 0 || !collision.IsTouching(damageBox[0]))
+                {
+                    if (collision.transform.position.x < transform.position.x)
+                    {
+                        ShellMove(true);
+                    }
+                    else if (collision.transform.position.x > transform.position.x)
+                    {
+                        ShellMove(false);
+                    }
+                    else if (collision.transform.position.x == transform.position.x)
+                    {
+                        ShellMove(true);
+                    }
+                }
+                else if(move)
+                {
+                    Debug.Log(name + ": deal damage");
+                }
             }
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        results = Physics2D.BoxCastAll(transform.position, transform.localScale, 0, new Vector2(walkSpeed, 0).normalized, 0.5f, lm);
+        results = Physics2D.BoxCastAll(transform.position, transform.localScale, 0, new Vector2(curSpeed, 0).normalized, 0.5f, lm);
         if (results.Length > 0)
         {
-            walkSpeed = -walkSpeed;
+            curSpeed = -curSpeed;
         }
     }
     private void OnDrawGizmos()
     {
-        Gizmos.DrawWireCube(transform.position + (new Vector3(walkSpeed, 0, 0).normalized * 0.5f), transform.localScale);
+        Gizmos.DrawWireCube(transform.position + (new Vector3(curSpeed, 0, 0).normalized * 0.5f), transform.localScale);
     }
 }
